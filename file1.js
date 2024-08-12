a9050293216bc2ff5da31e919ba7b3adfbcecfd9
Avdhesh813 <avdheshsharma813@gmail.com>
2024-08-12 11:38:22 +0530

    sdsdsd

---
+++ file1.js
@@ -0,0 +1,47 @@
+/*let a = 10;
+document.write(a, "<br/>")
+let b = 5;
+let sum = a+b;
+document.write(sum,"<br/>")
+let difference = a-b;
+document.write(difference,"<br/>")
+let product = a * b;
+document.write(product,"<br/>")
+let quotient = a / b;
+document.write(quotient,"<br/>")
+let remainder = a % b;
+document.write(remainder,"<br/>")
+let isequal = (a==b);
+document.write(isequal,"<br/>")
+let isnotequal = (a!=b);
+document.write(isnotequal,"<br/>")
+let isgreater = (a>b);
+document.write(isgreater,"<br/>")
+let logicland = (a>b && b < 10);
+document.write(logicland,"<br/>")*/
+
+
+/*let fname = "john"; //string
+'document.write("Hello ",fname,"<br/>")
+let age = 30; //number
+document.write("Age",age,"<br/>")
+let isstudent = true; //boolean
+document.write(`student - ${isstudent ? "Yes" : "No"}`," <br/>")
+let emptyvalue = null; //null
+document.write(`about us - ${emptyvalue || "write something"}`, "<br/>")
+let uninitialized; //undefined
+document.write(!uninitialized && "fill detail completely")*/
+
+
+/*let age = 18;
+if(age>17){
+    document.write("eligible for vote")
+}
+else{
+    document.write('not eligible for vote')
+}*/
+
+
+
+let age = 18;
+document.write(age>17 ? "eligible for vote" : "not eligible for vote")
--- javascript.js
+++ javascript.js
@@ -1,1 +1,20 @@
-document.write('<h1>Hiii</h1>')
+document.write('<h1>Hiii</h1>');
+var x = 12;
+var y = 13;
+document.write("Addition: ",x+y,"<br>");
+
+
+document.getElementById("demo").innerHTML="GLA University";
+
+
+console.log(10);
+
+function add(x,y) {
+    let c = x-y;
+    return c    
+}
+var a=1200;
+let c =123;
+const d =a+c;
+document.write(add(x,y),"<br>");
+document.write(d);
